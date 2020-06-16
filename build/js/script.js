70bdb3d38c90a8a6d9dc4885ab72f456aeada093
Griefman <stanislavparshin@yandex.ru>
2020-06-16 18:21:23 +0300

    Оживляет слайдер стренерами.

--- build/js/script.js
+++ build/js/script.js
@@ -1,24 +1,23 @@
 'use strict';
-/* Индекс слайда по умолчанию */
+var anchors = document.querySelectorAll('a[href*="#"]');
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
   showSlides(slideIndex += 1);
 }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+
 function minusSlide() {
   showSlides(slideIndex -= 1);
 }
 
-/* Устанавливает текущий слайд */
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-/* Основная функция слайдера */
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("reviews__slide");
@@ -34,3 +33,64 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "flex";
 }
 
+
+anchors.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
+    var blockID = item.getAttribute('href').substr(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+
+
+// ==============================================
+
+var slides = document.querySelectorAll(".trainers__item");
+var slidesNumber;
+var slideIndexTrainers = 0;
+var n;
+
+var width = parseInt(document.body.clientWidth);
+if (width > 1199) {
+  n = 4;
+} else if (width > 767 && width <= 1199) {
+  n = 2;
+} else {
+  n = 1;
+}
+
+slidesNumber = slides.length / n;
+function showNextSlide() {
+
+  if (slideIndexTrainers >= slidesNumber - 1 ) {
+
+  } else {
+    slideIndexTrainers += 1;
+    setStyle();
+  }
+}
+
+function showPreviewSlide() {
+  if (slideIndexTrainers <= 0) {
+
+  } else {
+    slideIndexTrainers -= 1;
+    setStyle();
+  }
+}
+
+
+function setStyle() {
+  var startIndex  = n * slideIndexTrainers;
+  var endIndex = startIndex + (n-1);
+  slides.forEach(function (item, index) {
+    if (index >= startIndex && index <= endIndex) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
