6fd6f2fe9dccdb3d569ef7a19b364860303781c5
Griefman <stanislavparshin@yandex.ru>
2020-06-16 18:32:12 +0300

    Оживляет слайдер стренерами.

--- source/js/script.js
+++ source/js/script.js
@@ -20,17 +20,17 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("reviews__slide");
+  var slides = document.getElementsByClassName('reviews__slide');
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
-  slides[slideIndex - 1].style.display = "flex";
+  slides[slideIndex - 1].style.display = 'flex';
 }
 
 
@@ -48,12 +48,12 @@ anchors.forEach(function (item) {
 
 // ==============================================
 
-var slides = document.querySelectorAll(".trainers__item");
+var slides = document.querySelectorAll('.trainers__item');
 var slidesNumber;
 var slideIndexTrainers = 0;
 var n;
 
-var width = parseInt(document.body.clientWidth);
+var width = parseInt(document.body.clientWidth, 10);
 if (width > 1199) {
   n = 4;
 } else if (width > 767 && width <= 1199) {
@@ -82,10 +82,9 @@ function showPreviewSlide() {
   }
 }
 
-
 function setStyle() {
-  var startIndex  = n * slideIndexTrainers;
-  var endIndex = startIndex + (n-1);
+  var startIndex = n * slideIndexTrainers;
+  var endIndex = startIndex + (n - 1);
   slides.forEach(function (item, index) {
     if (index >= startIndex && index <= endIndex) {
       item.style.display = 'block';
