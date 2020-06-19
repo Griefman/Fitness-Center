2747768e037c78f600edf838915dacbd7b98a874
Griefman <stanislavparshin@yandex.ru>
2020-06-20 01:23:20 +0300

    Добавляет свайп в слайдер Тренеры

--- source/js/script.js
+++ source/js/script.js
@@ -93,3 +93,47 @@ function setStyle() {
     }
   });
 }
+
+// ===================================================================
+// Вешаем на прикосновение функцию handleTouchStart
+var slider1 = document.querySelector('.trainers__list');
+// var slider2 = document.querySelector('.reviews');
+slider1.addEventListener('touchstart', handleTouchStart, false);
+// А на движение пальцем по экрану - handleTouchMove
+slider1.addEventListener('touchmove', handleTouchMove, false);
+
+// slider2.addEventListener('touchstart', handleTouchStart, false);
+// // А на движение пальцем по экрану - handleTouchMove
+// slider2.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function handleTouchStart(evt) {
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
+}
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+  // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( xDiff > 0 ) {
+      /* left swipe */
+      showPreviewSlide();
+    } else {
+      /* right swipe */
+      showNextSlide();
+    }
+  }
+  xDown = null;
+  yDown = null;
+}
