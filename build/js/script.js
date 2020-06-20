64c798f3a77e4340dcdd4288cd7f48cc505f69ca
Griefman <stanislavparshin@yandex.ru>
2020-06-20 19:41:10 +0300

    Исправляет обрезанный элемент

--- build/js/script.js
+++ build/js/script.js
@@ -96,15 +96,9 @@ function setStyle() {
 
 // ===================================================================
 // Вешаем на прикосновение функцию handleTouchStart
-var slider1 = document.querySelector('.trainers__list');
-// var slider2 = document.querySelector('.reviews');
+var slider1 = document.querySelector('.trainers');
 slider1.addEventListener('touchstart', handleTouchStart, false);
-// А на движение пальцем по экрану - handleTouchMove
 slider1.addEventListener('touchmove', handleTouchMove, false);
-
-// slider2.addEventListener('touchstart', handleTouchStart, false);
-// // А на движение пальцем по экрану - handleTouchMove
-// slider2.addEventListener('touchmove', handleTouchMove, false);
 
 var xDown = null;
 var yDown = null;
@@ -124,14 +118,44 @@ function handleTouchMove(evt) {
 
   var xDiff = xDown - xUp;
   var yDiff = yDown - yUp;
-  // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+
   if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-    if ( xDiff > 0 ) {
-      /* left swipe */
+    if ( xDiff > 0) {
       showPreviewSlide();
-    } else {
-      /* right swipe */
+    } else  {
       showNextSlide();
+    }
+  }
+  xDown = null;
+  yDown = null;
+}
+
+
+var slider2 = document.querySelector('.reviews');
+slider2.addEventListener('touchstart', handleTouchStart2, false);
+slider2.addEventListener('touchmove', handleTouchMove2, false);
+
+function handleTouchStart2(evt) {
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
+}
+
+function handleTouchMove2(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+    if ( xDiff > 0) {
+      minusSlide();
+    } else  {
+      plusSlide();
     }
   }
   xDown = null;
